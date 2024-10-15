e484f33da86cd0387a61b5dc267e6c73031fe65f
Jenarofg <jenarofg@gmail.com>
2024-10-16 01:03:00 +0200

    first commit

---
+++ categoriasVictoria.js
@@ -0,0 +1,8 @@
+
+
+let desde="2024-10-15"
+let hasta="2024-10-20"
+
+function busqueda(desde,hasta){
+    page=`https://www.asturfutbol.es/pnfg/NPcd/NFG_LstPartidos?cod_primaria=1000121&Consulta=1&Sch_Participantes_federados=&lista_competiciones_seleccionadas=&avanzada=&Sch_Fecha_Desde=${desde}&Sch_Fecha_Desde_input=${desde}&Sch_Fecha_Hasta=${hasta}&Sch_Fecha_Hasta_input=${hasta}&Sch_Tipo_Participantes=&Sch_Cod_Temporada=20&Sch_Codigo_Tipo_Juego=&Sch_Hora=&Sch_Cod_Agrupacion=&Sch_CodCompeticion=&Sch_CodGrupo=&texto_grupo=&Sch_codacta=&Sch_Clave_Acceso_Club=1168&Club=1168&Sch_Codigo_Equipo=&Sch_Clave_Acceso_Campo=&Campo=&Sch_Acta_Partido=&Sch_Partidos_Jugados=&Ordenacion=&NPcd_PageLines=20
+}
